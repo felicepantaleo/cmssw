cfb3207e62a15c50b13ee1d0a0dd658032c4a42e
AdrianoDee <adriano.diflorio@ba.infn.it>
2016-04-21 19:18:27 +0200

    CANtupleGenerator.cc

---
+++ RecoPixelVertexing/PixelTriplets/plugins/CANtupleGenerator.cc
@@ -0,0 +1,19 @@
+#include "RecoPixelVertexing/PixelTriplets/interface/CANtupleGenerator.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+CANtupleGenerator::CANtupleGenerator(unsigned int maxElement):
+theKDTReeCache(nullptr),
+theCACellsCache(nullptr),
+theMaxElement(maxElement)
+{}
+
+CANtupleGenerator::CANtupleGenerator(const edm::ParameterSet& pset):
+CANtupleGenerator(pset.getParameter<unsigned int>("maxElement"))
+{}
+
+CANtupleGenerator::~CANtupleGenerator() {}
+
+void CANtupleGenerator::init(LayerFKDTreeCache *kdTReeCache,LayersCACellsCache *caCellsCache) {
+    theKDTReeCache = kdTReeCache;
+    theCACellsCache = caCellsCache;
+}
