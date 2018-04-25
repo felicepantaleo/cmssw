91af5b683672ca6fa42c896b9f06502e49e0dd74
Felice <felice.pantaleo@cern.ch>
2018-04-25 15:24:59 +0200

    creating files for the GPU version of the Cellular Automaton

---
+++ RecoPixelVertexing/PixelTriplets/plugins/CAHitQuadrupletGeneratorGPU.cc
@@ -0,0 +1,84 @@
+#include "CAHitQuadrupletGeneratorGPU.h"
+
+#include "RecoPixelVertexing/PixelTriplets/interface/ThirdHitPredictionFromCircle.h"
+
+#include "DataFormats/Common/interface/Handle.h"
+#include "FWCore/Framework/interface/ConsumesCollector.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
+
+#include "TrackingTools/DetLayers/interface/BarrelDetLayer.h"
+
+#include "CellularAutomaton.h"
+
+#include "CommonTools/Utils/interface/DynArray.h"
+
+#include "FWCore/Utilities/interface/isFinite.h"
+
+#include <functional>
+
+namespace {
+
+template <typename T> T sqr(T x) { return x * x; }
+} // namespace
+
+using namespace std;
+
+constexpr unsigned int CAHitQuadrupletGeneratorGPU::minLayers;
+
+CAHitQuadrupletGeneratorGPU::CAHitQuadrupletGeneratorGPU(const edm::ParameterSet &cfg,
+                                                   edm::ConsumesCollector &iC)
+    : extraHitRPhitolerance(cfg.getParameter<double>(
+          "extraHitRPhitolerance")), // extra window in
+                                     // ThirdHitPredictionFromCircle range
+                                     // (divide by R to get phi)
+      maxChi2(cfg.getParameter<edm::ParameterSet>("maxChi2")),
+      fitFastCircle(cfg.getParameter<bool>("fitFastCircle")),
+      fitFastCircleChi2Cut(cfg.getParameter<bool>("fitFastCircleChi2Cut")),
+      useBendingCorrection(cfg.getParameter<bool>("useBendingCorrection")),
+      caThetaCut(cfg.getParameter<double>("CAThetaCut")),
+      caPhiCut(cfg.getParameter<double>("CAPhiCut")),
+      caHardPtCut(cfg.getParameter<double>("CAHardPtCut")) {
+  edm::ParameterSet comparitorPSet =
+      cfg.getParameter<edm::ParameterSet>("SeedComparitorPSet");
+  std::string comparitorName =
+      comparitorPSet.getParameter<std::string>("ComponentName");
+  if (comparitorName != "none") {
+    theComparitor.reset(SeedComparitorFactory::get()->create(
+        comparitorName, comparitorPSet, iC));
+  }
+}
+
+void CAHitQuadrupletGeneratorGPU::fillDescriptions(
+    edm::ParameterSetDescription &desc) {
+  desc.add<double>("extraHitRPhitolerance", 0.1);
+  desc.add<bool>("fitFastCircle", false);
+  desc.add<bool>("fitFastCircleChi2Cut", false);
+  desc.add<bool>("useBendingCorrection", false);
+  desc.add<double>("CAThetaCut", 0.00125);
+  desc.add<double>("CAPhiCut", 10);
+  desc.add<double>("CAHardPtCut", 0);
+  desc.addOptional<bool>("CAOnlyOneLastHitPerLayerFilter")
+      ->setComment(
+          "Deprecated and has no effect. To be fully removed later when the "
+          "parameter is no longer used in HLT configurations.");
+  edm::ParameterSetDescription descMaxChi2;
+  descMaxChi2.add<double>("pt1", 0.2);
+  descMaxChi2.add<double>("pt2", 1.5);
+  descMaxChi2.add<double>("value1", 500);
+  descMaxChi2.add<double>("value2", 50);
+  descMaxChi2.add<bool>("enabled", true);
+  desc.add<edm::ParameterSetDescription>("maxChi2", descMaxChi2);
+
+  edm::ParameterSetDescription descComparitor;
+  descComparitor.add<std::string>("ComponentName", "none");
+  descComparitor.setAllowAnything(); // until we have moved SeedComparitor too
+                                     // to EDProducers
+  desc.add<edm::ParameterSetDescription>("SeedComparitorPSet", descComparitor);
+}
+
+void CAHitQuadrupletGeneratorGPU::initEvent(const edm::Event &ev,
+                                         const edm::EventSetup &es) {
+  if (theComparitor)
+    theComparitor->init(ev, es);
+}
