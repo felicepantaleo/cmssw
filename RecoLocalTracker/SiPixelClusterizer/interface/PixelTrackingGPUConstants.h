d09b54ebeb46cc3e3168822b585e58acc4eff027
Felice <felice.pantaleo@cern.ch>
2018-09-11 17:31:24 +0200

    adding constants file

---
+++ RecoLocalTracker/SiPixelClusterizer/interface/PixelTrackingGPUConstants.h
@@ -0,0 +1,10 @@
+#ifndef RecoLocalTracker_SiPixelClusterizer_PixelTrackingGPUConstants_h
+#define RecoLocalTracker_SiPixelClusterizer_PixelTrackingGPUConstants_h
+
+#include <cstdint>
+
+namespace GPUConstants {
+  constexpr uint16_t maxNumberOfHits = 20000;
+
+}
+#endif
