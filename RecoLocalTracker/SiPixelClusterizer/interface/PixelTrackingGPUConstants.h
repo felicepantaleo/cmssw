969a2b605bca328ab8c5e86edd5d017bfdfa572f
Andrea Bocci <andrea.bocci@cern.ch>
2018-09-13 16:31:34 +0200

    Bump the maxNumberOfHits from 30k to 40k
    The distribution of the number of pixel hits for data at pileup 50 is reasonably Gaussian around 18300 ± 3500, so 30000 is only ~3.3 σ
    away from the average.
    Bump the maxNumberOfHits to 40000, which is > 6 σ away.

--- RecoLocalTracker/SiPixelClusterizer/interface/PixelTrackingGPUConstants.h
+++ RecoLocalTracker/SiPixelClusterizer/interface/PixelTrackingGPUConstants.h
@@ -1,10 +1,11 @@
-#ifndef RecoLocalTracker_SiPixelClusterizer_PixelTrackingGPUConstants_h
-#define RecoLocalTracker_SiPixelClusterizer_PixelTrackingGPUConstants_h
+#ifndef RecoLocalTracker_SiPixelClusterizer_interface_PixelTrackingGPUConstants_h
+#define RecoLocalTracker_SiPixelClusterizer_interface_PixelTrackingGPUConstants_h
 
 #include <cstdint>
 
 namespace PixelGPUConstants {
-  constexpr uint16_t maxNumberOfHits = 30000;
+  constexpr uint16_t maxNumberOfHits = 40000;       // data at pileup 50 has 18300 +/- 3500 hits; 40000 is around 6 sigma away
 
 }
-#endif
+
+#endif // RecoLocalTracker_SiPixelClusterizer_interface_PixelTrackingGPUConstants_h
