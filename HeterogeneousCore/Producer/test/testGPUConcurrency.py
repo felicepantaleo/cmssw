8bd12c310c45c243188c12df6a584fd503fd85a8
Andrea Bocci <andrea.bocci@cern.ch>
2018-10-06 10:19:57 +0200

    Update testGPUConcurrency.py

--- HeterogeneousCore/Producer/test/testGPUConcurrency.py
+++ HeterogeneousCore/Producer/test/testGPUConcurrency.py
@@ -1,19 +1,16 @@
 import FWCore.ParameterSet.Config as cms
 
-from Configuration.StandardSequences.Eras import eras
-
-process = cms.Process('RECO',eras.Run2_2018)
+process = cms.Process('RECO')
 
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('HeterogeneousCore.CUDAServices.CUDAService_cfi')
 
+# Empty source
+process.source = cms.Source("EmptySource")
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(10)
 )
-
-# Input source
-process.source = cms.Source("EmptySource",)
 
 process.options = cms.untracked.PSet(
     numberOfThreads = cms.untracked.uint32( 4 ),
@@ -23,9 +20,10 @@ process.options = cms.untracked.PSet(
 # Path and EndPath definitions
 from HeterogeneousCore.Producer.testHeterogeneousEDProducerGPU_cfi import testHeterogeneousEDProducerGPU
 process.testGPU = testHeterogeneousEDProducerGPU.clone()
-process.testGPU.sleep =1000000
-process.testGPU.blocks =100000
-process.testGPU.threads =256
+process.testGPU.sleep = 1000000
+process.testGPU.blocks = 100000
+process.testGPU.threads = 256
 
-process.p = cms.Path(process.testGPU)
-process.schedule = cms.Schedule(process.p)
+process.path = cms.Path(process.testGPU)
+
+process.schedule = cms.Schedule(process.path)
