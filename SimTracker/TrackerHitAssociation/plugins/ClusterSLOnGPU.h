e1f6196e9d6ca7ec1e302255d6009460d835b110
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2019-03-12 14:39:43 +0100

    add dump of zo,ro

--- SimTracker/TrackerHitAssociation/plugins/ClusterSLOnGPU.h
+++ SimTracker/TrackerHitAssociation/plugins/ClusterSLOnGPU.h
@@ -19,6 +19,8 @@ namespace clusterSLOnGPU {
   using HitsOnGPU    = siPixelRecHitsHeterogeneousProduct::HitsOnGPU;
   using HitsOnCPU    = siPixelRecHitsHeterogeneousProduct::HitsOnCPU;
 
+  using Clus2TP = ClusterSLGPU::Clus2TP;
+
   class Kernel {
   public:
     Kernel(cuda::stream_t<>& stream, bool dump);
