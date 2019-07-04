d7f94a920683b014beb99573f94f4f5e85d84733
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2019-07-04 18:02:02 +0200

    add more jumping forward doublets, retune cuts

--- RecoPixelVertexing/PixelTriplets/plugins/gpuPixelDoublets.h
+++ RecoPixelVertexing/PixelTriplets/plugins/gpuPixelDoublets.h
@@ -9,7 +9,7 @@ namespace gpuPixelDoublets {
 
   using namespace gpuPixelDoubletsAlgos;
 
-  constexpr int nPairs = 13+2+2;
+  constexpr int nPairs = 13+2+4;
   static_assert(nPairs<=CAConstants::maxNumberOfLayerPairs());
 
 // start constants
@@ -21,8 +21,8 @@ namespace gpuPixelDoublets {
       4,5, 7,8,      // FPIX1 (8)
       2,3, 2,4, 2,7, 5,6, 8,9,  // BPIX3 & FPIX2 (13)
       0,2, 1,3,      // Jumping Barrel (15)
-   // 0,5, 0,8,      // Jumping Forward (BPIX1,FPIX2)  OFF 
-      4,6, 7,9       // Jumping Forward (17)
+      0,5, 0,8,      // Jumping Forward (BPIX1,FPIX2)
+      4,6, 7,9       // Jumping Forward (19)
   };
 
   constexpr int16_t phi0p05 = 522;  // round(521.52189...) = phi2short(0.05);
@@ -34,12 +34,12 @@ namespace gpuPixelDoublets {
      phi0p05, phi0p06, phi0p06,
      phi0p05, phi0p05,
      phi0p06, phi0p06, phi0p06, phi0p05, phi0p05,
-     phi0p05, phi0p05, phi0p06,phi0p06};
-//   phi0p07, phi0p07, phi0p06,phi0p06};  // relaxed cuts
+     phi0p05, phi0p05, phi0p05,phi0p05, phi0p05,phi0p05};
+//   phi0p07, phi0p07, phi0p06,phi0p06, phi0p06,phi0p06};  // relaxed cuts
 
-  CONSTANT_VAR float const minz[nPairs] = {-20.,0.,-30., -22.,10.,-30., -70.,-70., -22.,15.,-30, -70.,-70., -20.,-22.,-70.,-70.};
-  CONSTANT_VAR float const maxz[nPairs] = { 20.,30.,0.,   22.,30.,-10.,  70.,70.,   22.,30.,-15., 70., 70.,  20.,22.,  70.,70.};
-  CONSTANT_VAR float const maxr[nPairs] = {20.,9.,9., 20.,7.,7., 5.,5., 20.,6.,6., 5., 5., 20.,20.,9.,9.}; 
+  CONSTANT_VAR float const minz[nPairs] = {-20.,0.,-30., -22.,10.,-30., -70.,-70., -22.,15.,-30, -70.,-70., -20.,-22., 0,-30., -70.,-70.};
+  CONSTANT_VAR float const maxz[nPairs] = { 20.,30.,0.,   22.,30.,-10.,  70.,70.,   22.,30.,-15., 70., 70.,  20.,22., 30.,0.,  70.,70.};
+  CONSTANT_VAR float const maxr[nPairs] = {20.,9.,9., 20.,7.,7., 5.,5., 20.,6.,6., 5., 5., 20.,20.,9.,9., 9.,9.}; 
 
 // end constants
 // clang-format on
