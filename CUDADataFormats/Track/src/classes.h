ba9c9b132d1b99df1cebe18433038b31586e329b
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2019-07-05 15:42:09 +0200

    compiles

---
+++ CUDADataFormats/Track/src/classes.h
@@ -0,0 +1,8 @@
+#ifndef CUDADataFormats__src_classes_h
+#define CUDADataFormats__src_classes_h
+
+#include "CUDADataFormats/Common/interface/CUDAProduct.h"
+#include "CUDADataFormats/Track/interface/PixelTrackCUDA.h"
+#include "DataFormats/Common/interface/Wrapper.h"
+
+#endif  
