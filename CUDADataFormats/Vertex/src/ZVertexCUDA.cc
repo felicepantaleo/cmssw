3b67cbcef2506c5d71616308b945b3363992c87f
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2019-07-07 17:30:19 +0200

    vertex format

---
+++ CUDADataFormats/Vertex/src/ZVertexCUDA.cc
@@ -0,0 +1,22 @@
+#include "CUDADataFormats/Vertex/interface/ZVertexCUDA.h"
+
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "HeterogeneousCore/CUDAServices/interface/CUDAService.h"
+#include "HeterogeneousCore/CUDAUtilities/interface/copyAsync.h"
+#include "HeterogeneousCore/CUDAUtilities/interface/cudaCheck.h"
+
+
+ZVertexCUDA::ZVertexCUDA(cuda::stream_t<> &stream) {
+  edm::Service<CUDAService> cs;
+  m_soa = cs->make_device_unique<SoA>(stream);
+}
+
+
+cudautils::host::unique_ptr<ZVertexCUDA::SoA> 
+ZVertexCUDA::soaToHostAsync(cuda::stream_t<>& stream) const{
+  edm::Service<CUDAService> cs;
+  auto ret = cs->make_host_unique<SoA>(stream);
+  cudaMemcpyAsync(ret.get(), m_soa.get(), sizeof(SoA), cudaMemcpyDefault, stream.id());
+  return ret;
+}
+
