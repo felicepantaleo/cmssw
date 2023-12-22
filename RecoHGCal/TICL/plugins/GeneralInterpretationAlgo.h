a42d08d2f55b856f0e0e91a142eb171c694f13d1
Wahid Redjeb <wahid.redjeb.wr@gmail.com>
2023-12-22 15:58:11 +0100

    ticlv5: implement general interpretation algo for linking tracks to tracksters

--- RecoHGCal/TICL/plugins/GeneralInterpretationAlgo.h
+++ RecoHGCal/TICL/plugins/GeneralInterpretationAlgo.h
@@ -7,25 +7,72 @@
 #include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
 #include "RecoHGCal/TICL/interface/TICLInterpretationAlgoBase.h"
 #include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/GeometrySurface/interface/BoundDisk.h"
 
 namespace ticl {
 
   class GeneralInterpretationAlgo : public TICLInterpretationAlgoBase<reco::Track> {
   public:
-    GeneralInterpretationAlgo(const edm::ParameterSet& conf, edm::ConsumesCollector iC)
-        : TICLInterpretationAlgoBase<reco::Track>(conf, iC) {
+    GeneralInterpretationAlgo(const edm::ParameterSet &conf, edm::ConsumesCollector iC);
 
-      }
+    ~GeneralInterpretationAlgo() override;
 
-  virtual ~GeneralInterpretationAlgo() {}
+    void makeCandidates(const Inputs &input,
+                        const TrackTimingInformation &inputTiming,
+                        std::vector<Trackster> &resultTracksters,
+                        std::vector<int> &resultCandidate) override;
 
-  virtual void makeCandidates(const Inputs& input, std::vector<Trackster>& resultTracksters,
-                                std::vector<TICLCandidate>& resultCandidate) override;
+    void initialize(const HGCalDDDConstants *hgcons,
+                    const hgcal::RecHitTools rhtools,
+                    const edm::ESHandle<MagneticField> bfieldH,
+                    const edm::ESHandle<Propagator> propH) override;
 
-  static void fillPSetDescription(edm::ParameterSetDescription& iDesc) {
-      iDesc.add<int>("algo_verbosity", 0);
-    }
+    static void fillPSetDescription(edm::ParameterSetDescription &iDesc);
 
+  private:
+    void buildLayers();
+
+    Vector propagateTrackster(const Trackster &t,
+                              const unsigned idx,
+                              float zVal,
+                              std::array<TICLLayerTile, 2> &tracksterTiles);
+
+    void findTrackstersInWindow(const MultiVectorManager<Trackster> &tracksters,
+                                const std::vector<std::pair<Vector, unsigned>> &seedingCollection,
+                                const std::array<TICLLayerTile, 2> &tracksterTiles,
+                                const std::vector<Vector> &tracksterPropPoints,
+                                float delta,
+                                unsigned trackstersSize,
+                                std::vector<std::vector<unsigned>> &resultCollection,
+                                bool useMask);
+
+    bool timeAndEnergyCompatible(float &total_raw_energy,
+                                 const reco::Track &track,
+                                 const Trackster &trackster,
+                                 const float &tkTime,
+                                 const float &tkTimeErr,
+                                 const float &tkTimeQual,
+                                 const float &tkBeta,
+                                 const GlobalPoint &tkMtdPos,
+                                 bool useMTDTiming);
+
+    const float tkEnergyCut_ = 2.0f;
+    const float maxDeltaT_ = 3.0f;
+    const float del_tk_ts_layer1_;
+    const float del_tk_ts_int_;
+    const float del_ts_em_had_;
+    const float del_ts_had_had_;
+
+    const float timing_quality_threshold_;
+    const HGCalDDDConstants *hgcons_;
+
+    std::unique_ptr<GeomDet> firstDisk_[2];
+    std::unique_ptr<GeomDet> interfaceDisk_[2];
+
+    hgcal::RecHitTools rhtools_;
+
+    edm::ESHandle<MagneticField> bfield_;
+    edm::ESHandle<Propagator> propagator_;
   };
 
 }  // namespace ticl
