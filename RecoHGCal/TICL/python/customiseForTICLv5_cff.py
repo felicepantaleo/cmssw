a8465c13de2015c86a1b7ac857caff50c0217227
Wahid Redjeb <wahid.redjeb.wr@gmail.com>
2023-12-29 10:57:42 +0100

    ticlv5: configure ticlv5 for validation

--- RecoHGCal/TICL/python/customiseForTICLv5_cff.py
+++ RecoHGCal/TICL/python/customiseForTICLv5_cff.py
@@ -14,6 +14,7 @@ from RecoHGCal.TICL.tracksterLinksProducer_cfi import tracksterLinksProducer as 
 from RecoHGCal.TICL.ticlCandidateProducer_cfi import ticlCandidateProducer as _ticlCandidateProducer
 from RecoHGCal.Configuration.RecoHGCal_EventContent_cff import customiseForTICLv5EventContent
 from RecoHGCal.TICL.iterativeTICL_cff import ticlIterLabels, ticlIterLabelsMerge
+from Validation.HGCalValidation.HGCalValidator_cff import hgcalValidatorv5 
 
 
 def customiseForTICLv5(process):
@@ -36,7 +37,24 @@ def customiseForTICLv5(process):
                                      process.ticlIterationsTask,
                                      process.ticlTracksterLinksTask,
                                      process.ticlCandidateTask)
+    process.particleFlowClusterHGCal.initialClusteringStep.tracksterSrc = "ticlCandidate"
+    process.globalrecoTask.remove(process.ticlTrackstersMerge)
+    process.tracksterSimTracksterAssociationLinking.label_tst = cms.InputTag("ticlCandidate")
+    process.tracksterSimTracksterAssociationPR.label_tst = cms.InputTag("ticlTracksterLinks")
+    process.tracksterSimTracksterAssociationLinkingbyCLUE3D.label_tst = cms.InputTag("ticlTracksterLinks")
+    process.tracksterSimTracksterAssociationPRbyCLUE3D.label_tst = cms.InputTag("ticlTracksterLinks")
+    process.tracksterSimTracksterAssociationLinkingPU.label_tst = cms.InputTag("ticlTracksterLinks")
+    process.tracksterSimTracksterAssociationPRPU.label_tst = cms.InputTag("ticlTracksterLinks")
     process.mergeTICLTask = cms.Task()
+    process.pfTICL.ticlCandidateSrc = cms.InputTag("ticlCandidate") 
+    process.hgcalValidatorv5 = hgcalValidatorv5.clone(
+        ticlTrackstersMerge = cms.InputTag("ticlCandidate"),
+        trackstersclue3d = cms.InputTag("ticlTracksterLinks")
+    )
+    process.hgcalValidatorSequence = cms.Sequence(process.hgcalValidatorv5)
+    process.hgcalValidation = cms.Sequence(process.hgcalSimHitValidationEE+process.hgcalSimHitValidationHEF+process.hgcalSimHitValidationHEB+process.hgcalDigiValidationEE+process.hgcalDigiValidationHEF+process.hgcalDigiValidationHEB+process.hgcalRecHitValidationEE+process.hgcalRecHitValidationHEF+process.hgcalRecHitValidationHEB+process.hgcalHitValidationSequence+process.hgcalValidatorSequence+process.hgcalTiclPFValidation+process.hgcalPFJetValidation)
+    process.globalValidationHGCal = cms.Sequence(process.hgcalValidation)
+    process.validation_step9 = cms.EndPath(process.globalValidationHGCal)
 
     process = customiseForTICLv5EventContent(process)
 
