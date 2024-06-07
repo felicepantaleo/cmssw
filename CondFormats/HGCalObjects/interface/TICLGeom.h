67925b2fa209eafe4cd2f1e1ec9572883ff51f34
Felice Pantaleo <felice.pantaleo@cern.ch>
2024-06-07 14:51:45 +0200

    ticlgeom under development

--- CondFormats/HGCalObjects/interface/TICLGeom.h
+++ CondFormats/HGCalObjects/interface/TICLGeom.h
@@ -1,18 +1,19 @@
 #ifndef CondFormats_HGCalObjects_TICLGeom_h
 #define CondFormats_HGCalObjects_TICLGeom_h
 
-#include "CondFormats/HGCalObjects/interface/HGCalGeomHostCollection.h"
+#include "CondFormats/HGCalObjects/interface/TICLGeomHostCollection.h"
+#include "CondFormats/HGCalObjects/interface/alpaka/TICLGeomDeviceCollection.h"
 #include "HeterogeneousCore/AlpakaInterface/interface/host.h"
 #include <map>
 #include <cstdint>
 
 struct TICLGeom {
   std::map<uint32_t, uint32_t> detIdToIndexMap;
-  std::unique_ptr<HGCalGeomHostCollection> hostCollection;
+  std::unique_ptr<TICLGeomHostCollection> hostCollection;
 
   TICLGeom() = delete;
   explicit TICLGeom(std::size_t size)
-      : hostCollection(std::make_unique<HGCalGeomHostCollection>(size, cms::alpakatools::host())) {}
+      : hostCollection(std::make_unique<TICLGeomHostCollection>(size, cms::alpakatools::host())) {}
 
   // Delete the copy constructor and copy assignment operator
   TICLGeom(const TICLGeom&) = delete;
