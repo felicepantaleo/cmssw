1798d14ea8b7ee0db11d8ff0722dbc6d21e8a501
Felice Pantaleo <felice.pantaleo@cern.ch>
2026-05-13 15:59:38 +0200

    Separate postprocessing

--- PhysicsTools/TruthInfo/python/truthGraphProducer_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-truthGraphProducer = cms.EDProducer(
-    "TruthGraphProducer",
-    # genParticles = cms.InputTag("genParticles"),
-    # simTracks    = cms.InputTag("g4SimHits"),
-)
