00a7e0bef9ebec7a5f0d749d24733956a1187af4
Felice Pantaleo <felice.pantaleo@cern.ch>
2026-05-13 10:22:25 +0200

    adding possibility to hide truth particles with no simhits

--- PhysicsTools/TruthInfo/python/truthLogicalGraphDumper_cfi.py
+++ PhysicsTools/TruthInfo/python/truthLogicalGraphDumper_cfi.py
@@ -1,11 +1,30 @@
-import FWCore.ParameterSet.Config as cms
-
-truthLogicalGraphDumper = cms.EDAnalyzer(
+process.truthLogicalGraphDumper = cms.EDAnalyzer(
     "TruthLogicalGraphDumper",
     src=cms.InputTag("truthLogicalGraphProducer"),
     rawSrc=cms.InputTag("truthGraphProducer"),
+    hitIndex=cms.InputTag("truthLogicalGraphHitIndexProducer"),
+
+    hgcalRecHits=cms.VInputTag(
+        cms.InputTag("HGCalRecHit", "HGCEERecHits", "RECO"),
+        cms.InputTag("HGCalRecHit", "HGCHEFRecHits", "RECO"),
+        cms.InputTag("HGCalRecHit", "HGCHEBRecHits", "RECO"),
+    ),
+
+    pfRecHits=cms.VInputTag(
+        cms.InputTag("particleFlowRecHitECAL", "Cleaned", "RECO"),
+        cms.InputTag("particleFlowRecHitHBHE", "Cleaned", "RECO"),
+        cms.InputTag("particleFlowRecHitHF", "Cleaned", "RECO"),
+        cms.InputTag("particleFlowRecHitHO", "Cleaned", "RECO"),
+    ),
+
     dotFile=cms.string("truthlogicalgraph.dot"),
-    maxParticles=cms.uint32(5000),
-    maxVertices=cms.uint32(5000),
-    maxEdgesPerNode=cms.uint32(200),
+
+    maxParticles=cms.uint32(20000),
+    maxVertices=cms.uint32(20000),
+    maxEdgesPerNode=cms.uint32(300),
+
+    hideLargeSimSourceVertices=cms.bool(True),
+    largeSimSourceVertexMinOutgoing=cms.uint32(50),
+
+    hideZeroSimHitSubgraphs=cms.bool(True),
 )
--- PhysicsTools/TruthInfo/test/dumpTruthGraphsFromGENSIMRECO_cfg.py
+++ PhysicsTools/TruthInfo/test/dumpTruthGraphsFromGENSIMRECO_cfg.py
@@ -97,11 +97,29 @@ process.truthLogicalGraphDumper = cms.EDAnalyzer(
     rawSrc=cms.InputTag("truthGraphProducer"),
     hitIndex=cms.InputTag("truthLogicalGraphHitIndexProducer"),
 
+    hgcalRecHits=cms.VInputTag(
+        cms.InputTag("HGCalRecHit", "HGCEERecHits", "RECO"),
+        cms.InputTag("HGCalRecHit", "HGCHEFRecHits", "RECO"),
+        cms.InputTag("HGCalRecHit", "HGCHEBRecHits", "RECO"),
+    ),
+
+    pfRecHits=cms.VInputTag(
+        cms.InputTag("particleFlowRecHitECAL", "Cleaned", "RECO"),
+        cms.InputTag("particleFlowRecHitHBHE", "Cleaned", "RECO"),
+        cms.InputTag("particleFlowRecHitHF", "Cleaned", "RECO"),
+        cms.InputTag("particleFlowRecHitHO", "Cleaned", "RECO"),
+    ),
+
     dotFile=cms.string("truthlogicalgraph.dot"),
 
     maxParticles=cms.uint32(20000),
     maxVertices=cms.uint32(20000),
     maxEdgesPerNode=cms.uint32(300),
+
+    hideLargeSimSourceVertices=cms.bool(True),
+    largeSimSourceVertexMinOutgoing=cms.uint32(50),
+
+    hideZeroSimHitSubgraphs=cms.bool(True),
 )
 
 process.MessageLogger.cerr.threshold = "INFO"
