42003aeb902549e82f3bc1f7a34085f1d8a84fbd
Felice Pantaleo <felice.pantaleo@cern.ch>
2026-07-18 12:27:26 +0200

    RecoTICL: v6 interpretation algos as distinct plugins, TICLv5 General preserved

--- RecoHGCal/TICL/plugins/TICLInterpretationPluginFactory.cc
+++ RecoHGCal/TICL/plugins/TICLInterpretationPluginFactory.cc
@@ -6,10 +6,17 @@
 #include "GeneralInterpretationAlgo.h"
 #include "GNNInterpretationAlgo.h"
 #include "MuonInterpretationAlgo.h"
+#include "ChargedHadronInterpretationAlgo.h"
+#include "EGammaInterpretationAlgo.h"
+#include "JetInterpretationAlgo.h"
 
 EDM_REGISTER_VALIDATED_PLUGINFACTORY(TICLGeneralInterpretationPluginFactory, "TICLGeneralInterpretationPluginFactory");
 EDM_REGISTER_VALIDATED_PLUGINFACTORY(TICLEGammaInterpretationPluginFactory, "TICLEGammaInterpretationPluginFactory");
 DEFINE_EDM_VALIDATED_PLUGIN(TICLGeneralInterpretationPluginFactory, ticl::GeneralInterpretationAlgo, "General");
 DEFINE_EDM_VALIDATED_PLUGIN(TICLGeneralInterpretationPluginFactory, ticl::GNNInterpretationAlgo, "GNNLink");
 DEFINE_EDM_VALIDATED_PLUGIN(TICLGeneralInterpretationPluginFactory, ticl::MuonInterpretationAlgo, "Muon");
-// DEFINE_EDM_VALIDATED_PLUGIN(TICLEGammaInterpretationPluginFactory, ticl::EGammaInterpretation, "EGamma");
+// TICLv6 (ticl_v6 arbitration chain): additive, distinct plugin names. "General" above
+// stays mapped to GeneralInterpretationAlgo so TICLv5 is unaffected.
+DEFINE_EDM_VALIDATED_PLUGIN(TICLGeneralInterpretationPluginFactory, ticl::ChargedHadronInterpretationAlgo, "ChargedHadron");
+DEFINE_EDM_VALIDATED_PLUGIN(TICLGeneralInterpretationPluginFactory, ticl::EGammaInterpretationAlgo, "EGamma");
+DEFINE_EDM_VALIDATED_PLUGIN(TICLGeneralInterpretationPluginFactory, ticl::JetInterpretationAlgo, "Jet");
